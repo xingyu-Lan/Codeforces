9cfb17e4668586073691249dda1b2bdaf5a64580
xingyu-Lan <99460937+xingyu-Lan@users.noreply.github.com>
2023-03-08 21:21:35 -0500

    Add files via upload

---
+++ 1669D.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+for i in range(int(input())):
+    n = int(input())
+    l = input().split('W')
+    bad = False
+    for s in l:
+        b1 = 'R' in s
+        b2 = 'B' in s
+        if (b1 ^ b2):
+            bad = True
+    print("NO" if bad else "YES")
+
