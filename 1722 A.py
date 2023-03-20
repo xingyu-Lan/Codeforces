cd33f0aa20fef11057be656da72289d6be84ba0f
xingyu-Lan <99460937+xingyu-Lan@users.noreply.github.com>
2023-03-20 17:45:24 -0400

    Add files via upload

---
+++ 1722 A.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+for _ in range(int(input())):
+    input()  # count of number of letters in the string
+    print ("YES" if sorted ("Tmiur")==sorted(input())else "NO")
+
